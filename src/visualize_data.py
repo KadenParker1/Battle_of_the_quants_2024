3386410a4b17f8aa23ba12196ee313afa31371c0
Kaden Parker <bobert11@byu.edu>
2024-09-27 18:41:07 -0600

    changed lengths of window

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
+pd.options.display.max_rows=999
+pd.options.display.max_columns=999
 
 # update this path to be your local path
 path='/Users/kadenparker/Desktop/Misc/quant/BOQ_data.parquet'
 
 df=pd.read_parquet(path)
 
-print(df.head())
+# print(df.loc[3530000])
+print(len(df["PERMNO"].unique()))
