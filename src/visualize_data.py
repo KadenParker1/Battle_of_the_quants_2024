56901b70a793ea88c5ccd0054aabee30a7f9b364
Andrew Criddle <acriddl2@byu.edu>
2024-10-05 23:08:11 -0600

    Isolate data-rich stocks for analysis.

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import numpy as np
 pd.options.display.max_rows=999
 pd.options.display.max_columns=999
 
 # update this path to be your local path
-path='/Users/kadenparker/Desktop/Misc/quant/BOQ_data.parquet'
+path='/Users/andrewcriddle/Desktop/Battle_of_the_quants_2024/data/Battle of the Quants Students/BOQ_data.parquet'
 
 df=pd.read_parquet(path)
 
-# print(df.loc[3530000])
-print(len(df["PERMNO"].unique()))
+df = df[df['PERMNO'].isin(df[df["DATE"] == np.datetime64('2023-12-31')]['PERMNO'])]
+df = df[df['PERMNO'].isin(df[df["DATE"] < np.datetime64('2005-01-31')]['PERMNO'])]
+df = df[df["PRC"].notna()]
+df = df[df["DATE"] >= np.datetime64('2005-01-31')]
+df = df.reset_index()
+
+
+print(df)
+print(len(df['PERMNO'].unique()))
+print(df[["PERMNO","DATE","RET",'ME','PRC']].head(999))
