cb39e12030ea1baff4bc1c70ecc8b9d6981f874a
Andrew Criddle <acriddl2@byu.edu>
2024-10-08 15:27:03 -0600

    Adapt.

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -9,12 +9,17 @@ path='/Users/andrewcriddle/Desktop/Battle_of_the_quants_2024/data/Battle of the 
 df=pd.read_parquet(path)
 
 df = df[df['PERMNO'].isin(df[df["DATE"] == np.datetime64('2023-12-31')]['PERMNO'])]
-df = df[df['PERMNO'].isin(df[df["DATE"] < np.datetime64('2005-01-31')]['PERMNO'])]
-df = df[df["PRC"].notna()]
-df = df[df["DATE"] >= np.datetime64('2005-01-31')]
-df = df.reset_index()
+df = df[df['PERMNO'].isin(df[df["DATE"] == np.datetime64('2005-01-31')]['PERMNO'])]
+# print(df[df["PRC"].isna()]['PERMNO'].unique())
+df = df[df['PERMNO'].isin(df[df["PRC"].isna()]['PERMNO'])]
+# [["PERMNO","DATE","RET",'ME','PRC']]
+print(df[["PERMNO","DATE","RET",'ME','PRC']])
+print(len(df["PERMNO"].unique()))
+# df = df[df["PRC"].notna()]
+# df = df[df["DATE"] >= np.datetime64('2005-01-31')]
+# df = df.reset_index()
 
 
-print(df)
-print(len(df['PERMNO'].unique()))
-print(df[["PERMNO","DATE","RET",'ME','PRC']].head(999))
+# print(df)
+# print(len(df['PERMNO'].unique()))
+# print(df[["PERMNO","DATE","RET",'ME','PRC']].head(999))
